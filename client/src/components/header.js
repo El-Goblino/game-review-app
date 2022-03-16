8f3c6549267bb094b019280906292a5fa76eacb2
Josh Haywood <jh248828@falmouth.ac.uk>
2022-03-16 20:55:26 +0000

    feat(header) Created basic navbar.

--- client/src/components/header.js
+++ client/src/components/header.js
@@ -1,7 +1,39 @@
 import * as React from 'react';
 
+function ToggleLinks() {
+  let hamburger = document.getElementById('hamburger');
+  let links = document.getElementById('links');
+
+  if (window.getComputedStyle(links).display === 'none') {
+    links.style.display = 'flex';
+  }
+
+  else {
+    links.style.display = 'none';    
+  }
+};
+
 export default function Header() {
   return (
-    <h1>Header</h1>
+    <nav class=" w-full bg-neutral-400">
+      <div class="container flex items-center justify-between px-3 py-2.5">
+        <a class="text-2xl" href="/">Logo</a>
+
+        <div id="hamburger" onClick={ToggleLinks} class="mb-1 space-y-2 block md:hidden">
+          <div class="w-6 h-0.5 bg-black"></div>
+          <div class="w-6 h-0.5 bg-black"></div>
+          <div class="w-6 h-0.5 bg-black"></div>
+        </div>
+
+        <ul id="links" class="hidden md:flex">
+          <li class="mx-1">
+            <a class="text-2xl" href="/">Home</a>
+          </li>
+          <li class="mx-1">
+            <a class="text-2xl" href="/reviews">Reviews</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
   )
 }
