0519c20e57a0cfafbbda613e8e80f984cf2d0503
JOSH Haywood <jh248828@falmouth.ac.uk>
2022-03-19 01:17:35 +0000

    refactor(featured, recent reviews) Refactored to use json variables for content.

--- client/src/components/index/featured.js
+++ client/src/components/index/featured.js
@@ -1,5 +1,18 @@
 import * as React from 'react';
 
+//Maps element based on the number of json objects
+const content = [
+    {'image': './images/elden-ring-thumbnail.jpg', 'heading': 'text1'},
+    {'image': './images/elden-ring-thumbnail.jpg', 'heading': 'text1'},
+];
+
+const items = content.map((content) =>
+    <div class="my-1 px-5 md:px-0 flex flex-col">
+        <img src={content.image} alt="Lorem Ipsum"></img> {/* Attribution: https://www.trustedreviews.com/news/is-sure-sounds-like-elden-ring-will-be-adapted-for-tv-or-movies-4218187 */}
+        <p class="bg-neutral-900 text-white py-1 px-2 font-semibold">{content.heading}</p>
+    </div>
+);
+
 export default function Featured() {
     return (
         <div>
@@ -13,15 +26,7 @@ export default function Featured() {
 
                 {/* Container for row flexing on medium screens */}
                 <div class="md:flex md:flex-row md:m-auto md:my-4 md:justify-center md:space-x-3 md:w-4/5 lg:w-3/5">
-                    <div class="my-1 px-5 md:px-0 flex flex-col">
-                        <img src="./images/elden-ring-thumbnail.jpg" alt="Lorem Ipsum"></img> {/* Attribution: https://www.trustedreviews.com/news/is-sure-sounds-like-elden-ring-will-be-adapted-for-tv-or-movies-4218187 */}
-                        <p class="bg-neutral-900 text-white py-1 px-2 font-semibold">Lorem Ipsum</p>
-                    </div>
-
-                    <div class="my-1 px-5 md:px-0 flex flex-col">
-                        <img src="./images/elden-ring-thumbnail.jpg" alt="Lorem Ipsum"></img> {/* Attribution: https://www.trustedreviews.com/news/is-sure-sounds-like-elden-ring-will-be-adapted-for-tv-or-movies-4218187 */}
-                        <p class="bg-neutral-900 text-white py-1 px-2 font-semibold">Lorem Ipsum</p>
-                    </div>
+                    <>{items}</>
                 </div>
             </div>
         </div>
--- client/src/components/index/recent-reviews.js
+++ client/src/components/index/recent-reviews.js
@@ -1,82 +1,32 @@
 import * as React from 'react';
 
+//Maps element based on the number of json objects
+const content = [
+    {'image': './images/elden-ring-thumbnail.jpg', 'heading': 'text1', 'text': 'text1'},
+    {'image': './images/elden-ring-thumbnail.jpg', 'heading': 'text1', 'text': 'text1'},
+    {'image': './images/elden-ring-thumbnail.jpg', 'heading': 'text1', 'text': 'text1'},
+    {'image': './images/elden-ring-thumbnail.jpg', 'heading': 'text1', 'text': 'text1'},
+    {'image': './images/elden-ring-thumbnail.jpg', 'heading': 'text1', 'text': 'text1'},
+    {'image': './images/elden-ring-thumbnail.jpg', 'heading': 'text1', 'text': 'text1'},
+];
+
+const items = content.map((content) =>
+    <div class="flex flex-row lg:flex-col mx-5 lg:mx-2 lg:mt-0 my-5 pb-5 items-center md:items-start lg:bg-white border-b-2 border-red-300">
+        <a href="#" class="w-[26%] lg:w-full mr-5 lg:mr-0 rounded-sm"><img src={content.image} alt="Lorem Impsum"></img></a>
+        <div class="md:flex md:flex-col lg:px-3">
+            <h6 class="font-bold lg:my-3">{content.heading}</h6>
+            <p class="hidden md:block my-2 lg:my-0">{content.text}</p>
+        </div>
+    </div>
+);
+
 export default function RecentReviews() {
     return (
         <div class="mx-1 lg:w-[60%] lg:m-auto">
             <h3 class="bg-red-500 font-bold text-white my-5 lg:my-0 px-4 py-0.5">Recent Reviews</h3>
 
             <div class="lg:grid grid-cols-3 grid-row-2">
-                {/* Article */}
-                <div class="flex flex-row lg:flex-col mx-5 lg:mx-2 lg:mt-0  my-5 pb-5 items-center md:items-start lg:bg-white border-b-2 border-red-300">
-                    {/* Thumbnail */}
-                    <a href="#" class="w-[26%] lg:w-full mr-5 lg:mr-0 rounded-sm"><img src="./images/elden-ring-thumbnail.jpg" alt="Lorem Impsum"></img></a>
-
-                    {/* Text */}
-                    <div class="md:flex md:flex-col lg:px-3">
-                        <h6 class="font-bold lg:my-3">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse sodales.</h6>
-                        <p class="hidden md:block my-2 lg:my-0">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse sodales.</p>
-                    </div>
-                </div>
-
-                {/* Article */}
-                <div class="flex flex-row lg:flex-col mx-5 lg:mx-2 lg:mt-0 my-5 pb-5 items-center md:items-start lg:bg-white border-b-2 border-red-300">
-                    {/* Thumbnail */}
-                    <a href="#" class="w-[26%] lg:w-full mr-5 lg:mr-0 rounded-sm"><img src="./images/elden-ring-thumbnail.jpg" alt="Lorem Impsum"></img></a>
-
-                    {/* Text */}
-                    <div class="md:flex md:flex-col lg:px-3">
-                        <h6 class="font-bold lg:my-3">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse sodales.</h6>
-                        <p class="hidden md:block my-2 lg:my-0">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse sodales.</p>
-                    </div>
-                </div>
-
-                {/* Article */}
-                   <div class="flex flex-row lg:flex-col mx-5 lg:mx-2 lg:mt-0 my-5 pb-5 items-center md:items-start lg:bg-white border-b-2 border-red-300">
-                    {/* Thumbnail */}
-                    <a href="#" class="w-[26%] lg:w-full mr-5 lg:mr-0 rounded-sm"><img src="./images/elden-ring-thumbnail.jpg" alt="Lorem Impsum"></img></a>
-
-                    {/* Text */}
-                    <div class="md:flex md:flex-col lg:px-3">
-                        <h6 class="font-bold lg:my-3">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse sodales.</h6>
-                        <p class="hidden md:block my-2 lg:my-0">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse sodales.</p>
-                    </div>
-                </div>
-
-                {/* Article */}
-                    <div class="flex flex-row lg:flex-col mx-5 lg:mx-2 lg:mt-0 my-5 pb-5 items-center md:items-start lg:bg-white border-b-2 border-red-300">
-                    {/* Thumbnail */}
-                    <a href="#" class="w-[26%] lg:w-full mr-5 lg:mr-0 rounded-sm"><img src="./images/elden-ring-thumbnail.jpg" alt="Lorem Impsum"></img></a>
-
-                    {/* Text */}
-                    <div class="md:flex md:flex-col lg:px-3">
-                        <h6 class="font-bold lg:my-3">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse sodales.</h6>
-                        <p class="hidden md:block my-2 lg:my-0">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse sodales.</p>
-                    </div>
-                </div>
-
-                {/* Article */}
-                <div class="flex flex-row lg:flex-col mx-5 lg:mx-2 lg:mt-0 my-5 pb-5 items-center md:items-start lg:bg-white border-b-2 border-red-300">
-                    {/* Thumbnail */}
-                    <a href="#" class="w-[26%] lg:w-full mr-5 lg:mr-0 rounded-sm"><img src="./images/elden-ring-thumbnail.jpg" alt="Lorem Impsum"></img></a>
-
-                    {/* Text */}
-                    <div class="md:flex md:flex-col lg:px-3">
-                        <h6 class="font-bold lg:my-3">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse sodales.</h6>
-                        <p class="hidden md:block my-2 lg:my-0">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse sodales.</p>
-                    </div>
-                </div>
-
-                {/* Article */}
-                <div class="flex flex-row lg:flex-col mx-5 lg:mx-2 lg:mt-0 my-5 pb-5 items-center md:items-start lg:bg-white border-b-2 border-red-300">
-                    {/* Thumbnail */}
-                    <a href="#" class="w-[26%] lg:w-full mr-5 lg:mr-0 rounded-sm"><img src="./images/elden-ring-thumbnail.jpg" alt="Lorem Impsum"></img></a>
-
-                    {/* Text */}
-                    <div class="md:flex md:flex-col lg:px-3">
-                        <h6 class="font-bold lg:my-3">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse sodales.</h6>
-                        <p class="hidden md:block my-2 lg:my-0">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse sodales.</p>
-                    </div>
-                </div>
+                <>{items}</>
             </div>
         </div>
     );
