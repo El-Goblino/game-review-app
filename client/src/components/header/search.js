2853e367653ad5bd68a746a0dfb094bd31ff4420
JOSH Haywood <jh248828@falmouth.ac.uk>
2022-03-21 14:23:07 +0000

    fix(search) If no input has been given user is no longer redirected.

--- client/src/components/header/search.js
+++ client/src/components/header/search.js
@@ -7,14 +7,17 @@ import { useNavigate } from "react-router-dom";
 export default function Search() {
   //For storing and setting search input
   const [query, setQuery] = useState("");
+  //For storing route path
   let navigate = useNavigate();
 
   function submitHandler() {
-    //If user hasnt searched anything on submit
+    //If user hasn't inputted a search on submit
     if (query.length === 0) {
       alert('Search bar is empty. Please enter a value to search.');
-    } else {
-      navigate(`/results/${query}`)
+    } 
+    //Else user has made input
+    else {
+      navigate(`/results/${query}`) //Redirect
     }
   }
 
@@ -37,8 +40,7 @@ export default function Search() {
 
         {/* Search Button */}
         {/* Flex container to align the icon and bar */}
-        <div className="flex mt-1.5">
-          <Link to={query.length === 0 ? "" : `/results/${query}`}>
+        <div className="flex">
             <button type="submit" onClick={submitHandler}>
               <svg
                 className="fill-current h-auto w-4 "
@@ -49,7 +51,6 @@ export default function Search() {
                 <path d="M500.3 443.7l-119.7-119.7c27.22-40.41 40.65-90.9 33.46-144.7C401.8 87.79 326.8 13.32 235.2 1.723C99.01-15.51-15.51 99.01 1.724 235.2c11.6 91.64 86.08 166.7 177.6 178.9c53.8 7.189 104.3-6.236 144.7-33.46l119.7 119.7c15.62 15.62 40.95 15.62 56.57 0C515.9 484.7 515.9 459.3 500.3 443.7zM79.1 208c0-70.58 57.42-128 128-128s128 57.42 128 128c0 70.58-57.42 128-128 128S79.1 278.6 79.1 208z" />
               </svg>
             </button>
-          </Link>
         </div>
       </form>
 
@@ -63,11 +64,8 @@ export default function Search() {
               if (query === "") {
                 return content;
               }
-
               //If any input characters much object characters return corresponding object
-              else if (
-                content.name.toLowerCase().includes(query.toLocaleLowerCase())
-              ) {
+              else if (content.name.toLowerCase().includes(query.toLocaleLowerCase())) {
                 return content;
               }
 
